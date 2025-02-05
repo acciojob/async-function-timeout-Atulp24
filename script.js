2b6b032c02c3fe3d1a3379b02b101996928e8c6d
Atulp24 <apk200209@gmail.com>
2025-02-05 19:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function delay(time) {
+	return new Promise(resolve => setTimeout(resolve, time));
+}
+async function delayedFunction() {
+	let dl = Number(document.getElementById("delay").value);
+	let tx = document.getElementById("text").value;
+	await delay(dl);
+	document.getElementById("output").innerHTML = tx;
+}
+document.getElementById("btn").addEventListener("click", delayedFunction);
